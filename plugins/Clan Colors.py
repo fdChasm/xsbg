fe73e7c860283f7f597475cf12362674dc490b2a
fdChasm <fd.chasm@gmail.com>
2011-03-19 18:06:10 -0700

    clan colors plugin now uses web location and default supports urls in config file, also hopefully some weird binary remnants are gone

--- plugins/Clan Colors.py
+++ plugins/Clan Colors.py
@@ -1,6 +1,6 @@
 from xsbg.players import extendPlayers, player
 from sbclient import *
-import random, os.path
+import random, os.path, urllib
 
 def bstrip(string):
 	"""helper function to remove spaces from Both beginning and end of the strings"""
@@ -21,28 +21,45 @@ class player_clan:
 	
 		self.clans = []
 
-		clan_colors_filename = os.path.expanduser(clan_colors_filename)
-	
-		f = open(clan_colors_filename, 'r')
-		lines = f.readlines()
-		#please ignore this bit of ugly code, it parses the login details file
-		for line in lines:
-			if line[0] == '#':
-				pass
-			else:
-				line = line.split("\t\t")
-				if len(line) > 1:
-					if line[1][-1:] == "\n":
-						line[1] = line[1][:-1]
-					line = map(bstrip, line)
-					line[1] = int(line[1], 16)
-					self.clans.append(clan(line[0], line[0], line[1]))
-		f.close()
+		self.read_config(clan_colors_filename)
 	
 		self.events = {
 			"connect": self.on_connect,
 			"rename": self.on_rename
 		}
+
+	def read_config(self, filename):
+		filename = os.path.expanduser(filename)
+
+		try:
+			if filename[0:4] == 'http':
+				f = urllib.urlopen(filename)
+			else:
+				f = open(filename, 'r')
+		
+			lines = f.readlines()
+			#please ignore this bit of ugly code, it parses the login details file
+			somelines = lines			
+			lines = None
+			lines = somelines
+
+			for line in lines:
+
+				if line[0] == '#':
+					pass
+				elif line[0] == '&':
+					self.read_config(line[1:])
+				else:
+					line = line.split("\t")
+					if len(line) > 1:
+						if line[1][-1:] == "\n":
+							line[1] = line[1][:-1]
+						line = map(bstrip, line)
+						line[1] = int(line[1], 16)
+						self.clans.append(clan(line[0], line[0], line[1]))
+			f.close()
+		except:
+			print "\tUnable to read Clan Color config file:", filename
 	
 	def on_connect(self, cn):
 		p = player(cn)
@@ -63,8 +80,9 @@ class player_clan:
 	def unload(self, players):
 		return True
 		
-player_clan_extension = player_clan("~/.sauerbraten/ClanColors.conf")
-		
+player_clan_extension = player_clan("http://forgottendream.org/xsbg/clan_colors.conf")
+#player_clan_extension = player_clan("~/.sauerbraten/clan_colors.conf")
+
 extendPlayers("clan_colors", player_clan_extension)
 
 print "\tClan colors plugin Now Active!"
